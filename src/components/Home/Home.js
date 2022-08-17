02495f36db22758edb49f887cfbbe5c4efedd93f
mss38 <mss38k@gmail.com>
2022-08-17 13:56:18 +0600

    navbar create completed

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -5,9 +5,7 @@ const Home = () => {
       <div>
          <h2>Home</h2>
          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum quidem enim repellat, deserunt iure aspernatur consequatur eos placeat at aut ipsa alias quod doloribus. Deleniti quos culpa facere inventore praesentium!</p>
-         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum quidem enim repellat, deserunt iure aspernatur consequatur eos placeat at aut ipsa alias quod doloribus. Deleniti quos culpa facere inventore praesentium!</p>
-         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum quidem enim repellat, deserunt iure aspernatur consequatur eos placeat at aut ipsa alias quod doloribus. Deleniti quos culpa facere inventore praesentium!</p>
-         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum quidem enim repellat, deserunt iure aspernatur consequatur eos placeat at aut ipsa alias quod doloribus. Deleniti quos culpa facere inventore praesentium!</p>
+       
       </div>
    );
 };
--- src/components/Shared/Navbar.js
+++ src/components/Shared/Navbar.js
@@ -5,40 +5,40 @@ const Navbar = ({ children }) => {
   const menuItems = (
     <>
       <li>
-        <Link to="/">Home</Link>
+        <Link className="hover:text-levender" to="/">Home</Link>
       </li>
       <li>
-        <Link to="/rent">Rent</Link>
+        <Link className="hover:text-levender" to="/rent">Rent</Link>
       </li>
       <li>
-        <Link to="/buy">Buy</Link>
+        <Link className="hover:text-levender" to="/buy">Buy</Link>
       </li>
       <li>
-        <Link to="/sell">Sell</Link>
+        <Link className="hover:text-levender" to="/sell">Sell</Link>
       </li>
       <li>
-        <Link to="/manage">Manage Properties</Link>
+        <Link className="hover:text-levender" to="/manage">Manage Properties</Link>
       </li>
       <li>
-        <Link to="/resources">resources</Link>
+        <Link className="hover:text-levender" to="/resources">resources</Link>
       </li>
       <li>
-        <Link to="/login">Login</Link>
+        <Link className="hover:text-levender" to="/login">Login</Link>
       </li>
       <li>
-        <Link to="/signup">Sign up</Link>
+        <Link className="hover:text-levender" to="/signup">Sign up</Link>
       </li>
     </>
   );
 
   return (
-    <div className="text-[#E0E6F0] bg-black">
+    <div className="text-grey bg-black">
       <div class="drawer">
         <input id="my-drawer-3" type="checkbox" class="drawer-toggle" />
         <div class="drawer-content flex flex-col ">
           <div class="w-full navbar bg-mirage text-steel  border border-x-0 border-[#1D2534] px-4 sm:px-8 lg:px-16 xl:px-20 2xl:px-24">
             <div class="flex-1 px-2 mx-2 text-2xl uppercase text-grey">
-              <Link to="/">Rent A Property</Link>
+              <Link className="hover:text-levender" to="/">Rent A Property</Link>
             </div>
             <div class="flex-none hidden lg:block">
               <ul class="menu menu-horizontal">{menuItems}</ul>
@@ -66,7 +66,7 @@ const Navbar = ({ children }) => {
         </div>
         <div class="drawer-side">
           <label for="my-drawer-3" class="drawer-overlay"></label>
-          <ul class="menu p-4 overflow-y-auto w-48 bg-base-100">{menuItems}</ul>
+          <ul class="menu p-4 overflow-y-auto w-48 bg-mirage text-steel">{menuItems}</ul>
         </div>
       </div>
     </div>
