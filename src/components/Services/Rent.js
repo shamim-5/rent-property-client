72ace439bd58a75305385fe63078d20f2690dd74
mss38 <mss38k@gmail.com>
2022-08-17 15:50:19 +0600

    card initial design done

--- src/components/Services/Rent.js
+++ src/components/Services/Rent.js
@@ -1,11 +1,62 @@
-import React from 'react';
+import React, { useState } from "react";
+import useServices from "../hooks/useServices";
 
 const Rent = () => {
-   return (
-      <div>
-         <h2>Rent Property</h2>
-      </div>
-   );
+  const [services, setServices] = useServices();
+  const [rent, setRent] = useState([]);
+
+  return (
+    <div className="container py-8 space-y-3">
+      <h2 className="text-3xl text-levender text-center uppercase">Rent Property</h2>
+      <section className="grid lg:grid-cols-3 gap-6">
+        {services &&
+          services.map((service) => {
+            const { id, location, date, price, type, name, address, bed, bath, size, img, review } = service;
+
+            return (
+              <div key={id}>
+                <div class="card card-compact w-92 bg-dark shadow-xl rounded-md">
+                  <figure>
+                    <img src={img} alt="" />
+                  </figure>
+                  <div class="card-body">
+                    <div className="flex justify-between items-center">
+                      <h2>
+                        <span className="font-bold text-secondary">${price}</span> /month
+                      </h2>
+                      <div>
+                        <p>Star</p>
+                      </div>
+                    </div>
+                    <h2 class="card-title text-levender">{name}</h2>
+                    <p>{address}</p>
+                    <hr />
+                    <div class="flex justify-between items-center">
+                      <div>
+                        <p>
+                          <span>{bed} Beds</span>
+                        </p>
+                      </div>
+                      <div>
+                        <p>
+                          <span>{bath} Bathrooms</span>
+                        </p>
+                      </div>
+                      <div>
+                        <p>
+                          <span>{size}m²</span>
+                        </p>
+                      </div>
+                      <button class="btn btn-primary">Buy Now</button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+      </section>
+    </div>
+  );
 };
 
 export default Rent;
