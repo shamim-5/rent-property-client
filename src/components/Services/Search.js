eae3f47eaebae1c58821b6c61313f3d8ce29b4fd
mss38 <mss38k@gmail.com>
2022-08-18 13:08:06 +0600

    search property implemented done

--- src/components/Services/Search.js
+++ src/components/Services/Search.js
@@ -1,20 +1,60 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const Search = ({ services, setServices }) => {
+  const [data, setData] = useState([]);
+  const [limit, setLimit] = useState(0);
+
+  useEffect(() => {
+    if (limit === 25000) {
+      const exist = data.filter((d) => d.price < limit);
+      setServices(exist);
+    } else if (limit === 50000) {
+      const exist = data.filter((d) => d.price < limit);
+      setServices(exist);
+    } else if (limit === 100000) {
+      const exist = data.filter((d) => d.price < limit);
+      setServices(exist);
+    } else if (limit === 20000000) {
+      const exist = data.filter((d) => d.price < limit);
+      setServices(exist);
+    } else {
+      setServices(data);
+    }
+  }, [data]);
+
   const handleSubmit = (e) => {
     e.preventDefault();
 
     const location = e.target.location.value;
     const date = e.target.date.value;
     const type = e.target.type.value;
-    // const price = e.target.price.value;
+    const price = e.target.price.value;
 
     fetch(`http://localhost:5000/service?location=${location}&date=${date}&type=${type}`, {
       method: "GET",
     })
       .then((res) => res.json())
-      .then((data) => console.log(data));
+      .then((data) => {
+        const shortData = [...data];
+
+        shortData.sort((a, b) => a.price.localeCompare(b.price, undefined, { numeric: true }));
+        setData(shortData);
+      });
+
+    if (price === "$0000-$25000") {
+      return setLimit(25000);
+    } else if (price === "$25000-$50000") {
+      return setLimit(50000);
+    } else if (price === "$50000-$100000") {
+      return setLimit(100000);
+    } else if (price === "$100000-$20000000") {
+      return setLimit(20000000);
+    } else {
+      console.log("Invalid price");
+    }
   };
+
+  console.log(services);
 
   return (
     <div>
